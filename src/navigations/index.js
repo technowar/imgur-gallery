e49eefb7baffbd921285892111c8b254bd962544
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2019-09-07 07:43:33 +0800

    Env local

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Config from 'config.json';
 import Layout from 'components/layout';
 import Album from 'containers/album';
 import Index from 'containers/index';
@@ -8,7 +7,7 @@ import { getSessionStorage, setSessionStorage } from 'helpers';
 
 export default () => {
   if (!getSessionStorage('client')) {
-    setSessionStorage('client', Config.clientId || process.env.REACT_APP_IMGUR_CLIENT_ID);
+    setSessionStorage('client', process.env.REACT_APP_IMGUR_CLIENT_ID);
   }
 
   return (
